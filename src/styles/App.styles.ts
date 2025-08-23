cc6d2e8392317e70f75c40811595b381447af38f
Tuominen <arttutuominen10@gmail.com>
2025-08-23 14:13:07 +0300

    update dependencies and add new styles for loading and header components. Add simple test for test Actions

---
+++ src/styles/App.styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  loadingText: {
+    marginTop: 20,
+    fontSize: 16,
+    color: '#666',
+  },
+  headerButton: {
+    marginRight: 15,
+    padding: 8,
+  },
+  headerButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
